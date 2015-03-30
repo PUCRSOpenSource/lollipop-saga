b0af22a412f992d6ff4db0e3f5d1016e0aee3661
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-03-30 20:46:38 -0300

    This Closes #1

--- game/src/Ship.cpp
+++ game/src/Ship.cpp
@@ -15,8 +15,8 @@ void Ship::draw()
         glColor3f(1,0,0);
         glLineWidth(3);
         glBegin(GL_TRIANGLES);
-        glVertex2f(-0.2f,-0.30f);
-        glVertex2f(0.0f,0.30f);
-        glVertex2f(0.2f,-0.30f);
+        glVertex2f(-0.1f,-0.80f);
+        glVertex2f(0.0f,-0.70f);
+        glVertex2f(0.1f,-0.80f);
         glEnd();
 }
