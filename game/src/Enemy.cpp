3601aec59db3f22006ab7c6fc68970cef1f6d80a
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-04-15 18:19:37 -0300

    Implements Enemy.cpp

---
+++ game/src/Enemy.cpp
@@ -0,0 +1,39 @@
+#include "Enemy.h"
+#include <cmath>
+
+Enemy::Enemy(void) : GameObject()
+{
+        setX(0.0f);
+        setX(-0.6f);
+}
+
+void Enemy::draw()
+{
+        glColor3f(0,0,1);
+        //FIXME
+        //colocar cx e cx em um ponto
+        float cx = this->x;
+        float cy = this->y;
+
+        int segs = 100;
+        float t;
+
+        float theta = 2.0f * M_PI / float(segs);
+        float cos_theta = cosf(theta);
+        float sin_theta = sinf(theta);
+        float r = 0.1f;
+
+        float posX = r;
+        float posY = 0;
+
+        glLineWidth(2);
+        glBegin(GL_LINE_LOOP);
+        for(int i = 0; i < segs; i++)
+        {
+            glVertex2f(posX + cx, posY + cy);
+            t = posX;
+            posX = cos_theta * posX - sin_theta * posY;
+            posY = sin_theta * t + cos_theta * posY;
+        }
+        glEnd();
+}
